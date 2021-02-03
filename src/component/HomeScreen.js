c9c1de6c0949815d22112797f0f9af20341f92e0
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 00:15:05 +0530

    add Nav.js and  Nav.css in component

--- src/component/HomeScreen.js
+++ src/component/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./HomeScreen.css";
+import Nav from "./Nav";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
-      <h1>home Screen</h1>
+      <Nav />
     </div>
   );
 }
---
+++ src/component/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h1>i am Nav</h1>
+    </div>
+  );
+}
+
+export default Nav;
