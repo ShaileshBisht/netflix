05a967980a897678a45d50aaba631ca31be32428
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 01:00:34 +0530

    add banner content

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -10,7 +10,18 @@ function Banner() {
         backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
         backgroundPosition: "center center",
       }}
-    ></header>
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">My List</button>
+          <button className="banner_button">Play</button>
+        </div>
+        <h1 className="banner_discription">this is a test discription</h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
   );
 }
 
