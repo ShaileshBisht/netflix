cf41b04e05025a956746e72019d8a24ee66f72fc
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 00:29:53 +0530

    add new netflix logo and add style to logo nad avatar

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
       <div className="nav_contents">
         <img
           className="nav_logo"
-          src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
 
