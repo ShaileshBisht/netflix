eb62cf77faaa0db1f459b794375df01f784d9989
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 00:10:45 +0530

    add HomeScreen.js and  HomeScreen.css in component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import HomeScreen from "./component/HomeScreen";
 
 function App() {
   return (
     <div className="app">
-      <h1>lets build netflix</h1>
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/component/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <h1>home Screen</h1>
+    </div>
+  );
+}
+
+export default HomeScreen;
