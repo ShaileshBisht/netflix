7aa029a0f5ef20bcc94bc53698d6154177a998bd
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 23:59:19 +0530

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLOhslBprDo4iQX9hK7qsCcAAjzFfrnPE",
+  authDomain: "netflix-clone-27e67.firebaseapp.com",
+  projectId: "netflix-clone-27e67",
+  storageBucket: "netflix-clone-27e67.appspot.com",
+  messagingSenderId: "159189847160",
+  appId: "1:159189847160:web:ee73353f77378b42e9b18d",
+};
