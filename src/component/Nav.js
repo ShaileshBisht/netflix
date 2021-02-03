77b806f84c28ac3a1cbbd548b0294b4419465d82
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 00:23:24 +0530

    add netflix logo and avatar

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -4,7 +4,19 @@ import "./Nav.css";
 function Nav() {
   return (
     <div className="nav">
-      <h1>i am Nav</h1>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024"
+          alt=""
+        />
+
+        <img
+          className="nav_avatar"
+          src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
