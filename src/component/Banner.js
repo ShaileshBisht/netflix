ab219e9675254f88e4e130c9f6c9582a11cbde06
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 00:47:35 +0530

    add Banner.js and Banner.css

---
+++ src/component/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>i am banner</h1>
+    </div>
+  );
+}
+
+export default Banner;
--- src/component/HomeScreen.js
+++ src/component/HomeScreen.js
@@ -1,51 +1,13 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
+      <Banner />
     </div>
   );
 }
