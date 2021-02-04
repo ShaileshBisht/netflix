2aaf1b8a5b34b34d26c95150b8e60a2ca76ea94f
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 01:59:54 +0530

    add h4 and  style it in form in SignupScreen component

--- src/component/SignupScreen.js
+++ src/component/SignupScreen.js
@@ -9,6 +9,10 @@ function SignupScreen() {
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <button type="submit">Sign In</button>
+        <h4>
+          <span className="signupScreen_gray">New to netflix? </span>
+          Sign Up now.
+        </h4>
       </form>
     </div>
   );
