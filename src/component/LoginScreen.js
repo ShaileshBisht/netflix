ada2dd31d623da76715881bf7e1bdd4ad7b5ae2c
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 00:30:26 +0530

    add sign in button  in  LoginScreen.js

--- src/component/LoginScreen.js
+++ src/component/LoginScreen.js
@@ -10,6 +10,7 @@ function Login() {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
+        <button className="loginScreen_button">Sign In</button>
       </div>
     </div>
   );
