608e5eb4d5237072fc3ec690028b793fb7271049
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 01:33:36 +0530

    add form to SignupScreen component

--- src/component/SignupScreen.js
+++ src/component/SignupScreen.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./SignupScreen.css";
 
 function SignupScreen() {
-  return <div className="signupScreen"></div>;
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignupScreen;
