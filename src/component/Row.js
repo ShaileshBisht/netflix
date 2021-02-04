c3b1d52b3b48c3d32aa06eabb0450f037c0465be
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 18:50:48 +0530

    set movies img in row

--- src/component/Row.js
+++ src/component/Row.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "../axios";
-import requests from "../Requests";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,11 +13,21 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
       setMovies(request.data.results);
       return request;
     };
+
+    fetchData();
   }, [fetchUrl]);
 
   return (
     <div className="row">
       <h2>{title}</h2>
+      {movies.map((movie) => (
+        <img
+          src={`${base_url}${
+            isLargeRow ? movie.poster_path : movie.backdrop_path
+          }`}
+          alt={movie.name}
+        />
+      ))}
     </div>
   );
 }
