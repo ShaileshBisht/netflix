8aada47076bd659ce1c14ab3d0376c7d08786d67
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 00:03:13 +0530

    add firebase and react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomeScreen from "./component/HomeScreen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
