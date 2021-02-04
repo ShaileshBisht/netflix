f09affa8a50b4e2973ef316a7ed33ac9506846a8
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 02:37:11 +0530

    add auth.onAuthStateChange to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./component/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./component/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        console.log(userAuth);
+      } else {
+        //logged out
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
