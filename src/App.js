b24c4c55a0d0485b01ce78faa271eea567d84f0b
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 00:18:58 +0530

    add Login.js and Login.css to component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./component/HomeScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./component/Login";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <Login />
+        ) : (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </div>
   );
---
+++ src/component/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h1>i am login</h1>
+    </div>
+  );
+}
+
+export default Login;
