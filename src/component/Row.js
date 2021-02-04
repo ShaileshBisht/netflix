2248f3aa3d5bbddb2df6172864fd1ccaf50b11b8
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 19:20:26 +0530

    add condition to poster path

--- src/component/Row.js
+++ src/component/Row.js
@@ -21,16 +21,20 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            key={movie.id}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   );
