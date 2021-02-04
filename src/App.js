3bc5e406caabe044585b67a90d085cfa2861253a
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 00:10:25 +0530

    add HomeSreen.js to react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
