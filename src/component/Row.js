25db3e4b1477dd44f555e61c9cc9e68a5c8e93fe
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-04 18:42:42 +0530

    fetch movies in useEffect using url send in prop

--- src/component/Row.js
+++ src/component/Row.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Row.css";
+import axios from "../axios";
+import requests from "../Requests";
 
-function Row({ title }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    };
+  }, [fetchUrl]);
+
   return (
     <div className="row">
       <h2>{title}</h2>
