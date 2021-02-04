a2dd4074aa5b5d69dd9f77caca7f245666a32c5f
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 02:07:39 +0530

    add onClick function in form in SignupScreen

--- src/component/SignupScreen.js
+++ src/component/SignupScreen.js
@@ -2,16 +2,28 @@ import React from "react";
 import "./SignupScreen.css";
 
 function SignupScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <button onClick={signIn} type="submit">
+          Sign In
+        </button>
         <h4>
           <span className="signupScreen_gray">New to netflix? </span>
-          <span className="signupScreen_link">Sign Up now.</span>
+          <span className="signupScreen_link" onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
