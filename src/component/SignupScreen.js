4fa3b044a7fedfee3d03c89fcac5015f919a44eb
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 02:28:27 +0530

    add signIn functionality to sign in button page

--- src/component/SignupScreen.js
+++ src/component/SignupScreen.js
@@ -24,6 +24,15 @@ function SignupScreen() {
 
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
