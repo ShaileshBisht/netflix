c71240dc2cda7c820b3e6f1d21627df9026524be
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 03:15:21 +0530

    add Nav.js and profileScreen_info to profileScreen

--- src/component/ProfileScreen.js
+++ src/component/ProfileScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 import Nav from "./Nav";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="profileScreen">
       <Nav />
+      <div className="profileScreen_body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen_info">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt=""
+          />
+          <div className="profileScreen_details">
+            <h2>{user.email}</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
