29082df357afe776d4954dc702ff393e2f6666a7
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-05 02:22:39 +0530

    add register functionality to sign up page

--- src/component/SignupScreen.js
+++ src/component/SignupScreen.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
+import { auth } from "../firebase";
 import "./SignupScreen.css";
 
 function SignupScreen() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
@@ -14,8 +30,8 @@ function SignupScreen() {
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
         <button onClick={signIn} type="submit">
           Sign In
         </button>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDLOhslBprDo4iQX9hK7qsCcAAjzFfrnPE",
   authDomain: "netflix-clone-27e67.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "159189847160",
   appId: "1:159189847160:web:ee73353f77378b42e9b18d",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
